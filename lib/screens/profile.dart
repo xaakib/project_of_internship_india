4bb8a56b544b159d5cc468db3bd269b70fd7ca1c
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-10 23:42:49 +0600

    cnage some

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -4,7 +4,44 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.red,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.zero,
+                      topRight: Radius.zero,
+                      bottomLeft: Radius.circular(150),
+                      bottomRight: Radius.circular(150),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 150,
+                        width: 150,
+                        child: Image.network(
+                          "https://images.unsplash.com/photo-1610256662599-49ff2e9570ea?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  )),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
